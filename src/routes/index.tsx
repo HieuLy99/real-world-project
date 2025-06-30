38d91330e161b4f438729129dd03603845c29579
HieuLy99 <lytrunghieuhm@gmail.com>
2025-06-30 23:13:29 +0700

    setup: add file for feature login + sign up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import LoginPage from '../pages/Login';
 
 export const router = createBrowserRouter([
+    {
+    path: '/',
+    element: <LoginPage />,
+    children: [
+      {
+        index: true,
+      }
+    ]
+  },
   {
     path: '/test',
     element: <App />,
